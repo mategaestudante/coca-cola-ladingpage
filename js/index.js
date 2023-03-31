f3d0df39f60e1cd8a8f9f2d4a3bc63ccae22abc8
mategaestudante <matheuspaixaodf@hotmail.com>
2023-03-31 17:39:23 -0300

    mais um site
    Uso de HTML E CSS E UM POUCO E PRIMEIRO SISTEMINHA EM JS, MENU MOBILE E TROCA DE ITEMS NA INTRODUÇÃO

---
+++ js/index.js
@@ -0,0 +1,44 @@
+let foto = window.document.getElementById('foto')
+let bg = window.document.getElementById('bgd-img')
+function mudar(){
+    foto.src = 'imagens/e3a9391d-e681-41ba-9d42-a645d355445f-removebg-preview.png'
+    bg.style.backgroundColor = '#a9adab'
+  
+
+}
+function mudar2 (){
+
+   
+    foto.src =  'imagens/fanta-lata-removebg-preview.png'
+    bg.style.backgroundColor = '#dd7222'
+
+
+   
+
+}
+function mudar3 (){
+
+
+     foto.src =  'imagens/coca-lata-removebg-preview.png'
+     bg.style.backgroundColor = '#262626'
+
+
+
+
+}
+function mudar4 (){
+    foto.src = 'imagens/coca-normal-removebg-preview.png'
+    bg.style.backgroundColor = '#a71f1f'
+}
+let menu = window.document.getElementById('menu')
+let sb = window.document.getElementById('side-bar')
+function ativar (){
+
+    sb.style.display = 'block'
+
+}
+function desativar(){
+
+    sb.style.display = 'none'
+
+}
